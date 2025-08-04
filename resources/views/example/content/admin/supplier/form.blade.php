7d926c17c5ae53c2e3c30b6884721bfc5d0abd9f
Ebenhzr06 <ebenhzr06@gmail.com>
2025-08-04 19:49:29 +0700

    form.blade.php

--- resources/views/example/content/admin/supplier/form.blade.php
+++ resources/views/example/content/admin/supplier/form.blade.php
@@ -11,14 +11,14 @@
                         Name</label>
                     <input type="text" name="name" id="name"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="Type product name" required="" value="{{ isset($supplier) ? $supplier->name : '' }}">
+                        placeholder="Supplier name" required="" value="{{ isset($supplier) ? $supplier->name : '' }}">
                 </div>
                 <div>
                     <label for="address"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
                     <input type="text" name="address" id="address"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="Type product Category" required=""
+                        placeholder="Your address" required=""
                         value="{{ isset($supplier) ? $supplier->address : '' }}">
                 </div>
                 <div>
@@ -26,18 +26,18 @@
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                     <input type="text" name="email" id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="Type product Category" required=""
+                        placeholder="Email" required=""
                         value="{{ isset($supplier) ? $supplier->email : '' }}">
                 </div>
                 <div>
                     <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Phone</label>
                     <input type="number" name="phone" id="phone"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="number phone" required="" value="{{ isset($supplier) ? $supplier->phone : '' }}">
+                        placeholder="Number phone" required="" value="{{ isset($supplier) ? $supplier->phone : '' }}">
                 </div>
                     <div  class="bottom-0 left-0 flex  w-full p-4 space-x-2 whitespace-nowrap">
                         <button type="submit" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                   {{ isset($supplier) ? 'Save Product' : 'Add Product' }}
+                   {{ isset($supplier) ? 'Save Supplier' : 'Add Supplier' }}
                 </button>
                 <br>
                 <a href="{{ route('Admin.supplier.index')}}"><button type="button" data-drawer-dismiss="drawer-create-product-default" aria-controls="drawer-create-product-default" class="inline-flex px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
