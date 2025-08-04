2591a6597d216f39146291720dc493819ddab8d6
Ebenhzr06 <ebenhzr06@gmail.com>
2025-08-04 19:47:55 +0700

    form.blade.php

--- resources/views/example/content/admin/supplier/form.blade.php
+++ resources/views/example/content/admin/supplier/form.blade.php
@@ -33,7 +33,7 @@
                     <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Phone</label>
                     <input type="number" name="phone" id="phone"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="$2999" required="" value="{{ isset($supplier) ? $supplier->phone : '' }}">
+                        placeholder="number phone" required="" value="{{ isset($supplier) ? $supplier->phone : '' }}">
                 </div>
                     <div  class="bottom-0 left-0 flex  w-full p-4 space-x-2 whitespace-nowrap">
                         <button type="submit" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
